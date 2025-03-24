8ee67da60fd62338c02aed25c1d3f73b515da2cb
KyleMacklane <kylemacklane@gmail.com>
2025-03-24 18:31:02 +0000

    'removed unnecessary files'

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api"; 
+const API_URL = "https://flushserver.onrender.com/api"; 
 
 // ✅ Clients API
 export const getClients = async () => {
